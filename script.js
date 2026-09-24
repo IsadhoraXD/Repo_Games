f25dfbbddf761b380913fe6b8d1703d5781f51cd
Isadhora Arendt <isadhoraarendt@gmail.com>
2026-09-24 09:09:37 -0300

    Mudanças entrega 1

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 const canvas = document.getElementById("game")
 const ctx = canvas.getContext("2d");
 
-const player = {x: 40, y: 160, w: 32, h:32, vx:120}
+const player = {x: 40, y: 160, r:40, vx:120, vy:120}
 
 let last = 0;
 
-function update(dt){
+function update(dt){ // Eu usei dt porque ele normaliza a velocidade, 
+// fazendo com que o player (nesse caso a bola) se mova da mesma forma independente do FPS. 
     player.x += player.vx * dt;
-    if (player.x < 0 || player.x + player.w > canvas.width){
+    player.y += player.vy * dt;
+    if (player.x - player.r < 0 || player.x + player.r > canvas.width){
         player.vx *= -1;
+    }    
+    if (player.y - player.r < 0 || player.y + player.r > canvas.height){
+        player.vy *= -1;
     }
 }
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#4ade80";
-    ctx.fillRect(player.x, player.y, player.h, player.w);
-
     ctx.fillStyle = "#fff";
-    ctx.fillRect(player.x, player.y, player.h, player.w);
-    
-    ctx.fillText("O Deltaine - dt independente de taxa de quadros", 12, 20);
+    ctx.beginPath();
+    ctx.arc(player.x, player.y, player.r, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.fillStyle = "#fff";    
+    ctx.fillText("Delta time - dt independente de taxa de quadros", 12, 20);
 }
 
 function loop(ts){
     if(!last) last = ts;
 
-    const dt = Math.min(0.05, (ts - last/1000)); // ms = segundo
+    const dt = Math.min(0.05, (ts - last)/1000); // ms = segundo
     last = ts;
     update(dt);
     draw();
